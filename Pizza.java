5678ccf73cc51158d6a870ea1c663d84022e0e49
Rahul <rktechnicalgroup69@gmail.com>
2023-08-13 23:15:17 +0530

    making a pera constructor in pizza class

--- Pizza.java
+++ Pizza.java
@@ -2,4 +2,8 @@ class Pizza{
 	private Double pizzaPrice;
 	private String pizzaType;
 	
+	public Pizza(Double pizzaPrice,String pizzaType){
+		this.pizzaPrice = pizzaPrice;
+		this.pizzaType = pizzaType;
+	}
 }
